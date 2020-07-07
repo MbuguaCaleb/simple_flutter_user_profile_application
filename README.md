2970f912ac5a1f29a6e82db3202300d99ee28357
MbuguaCaleb <MbuguaCaleb>
2020-07-07 05:00:18 +0300

    'simple-flutter-application'

--- README.md
+++ README.md
@@ -5,6 +5,43 @@
 
 (b)You may consider to put a padding widget to wrap around other child
 widgets eg the Column
+
+(c)TextStyle is used to style a text widget .
+
+(d)Edge insets relates to the space around a margin or padding.
+      
+```
+
+**Snippet**
+```
+     Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Mbugua-Caleb',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            )
+
+
+sizedBox is like a <br> in html nd css.
+it can be used to create spacing for both width and height
+(e)When i want to arranage elements horizontally i should consider a row.
+
+(f)Widgets have got properties and you can use the various properties to modify a given widget.
+
+(g)There are some widgets that take in others example padding and center.
+
+
 ```
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -25,14 +25,72 @@ class CalebCard extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/caleb.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
             Text(
               'NAME',
               style: TextStyle(
                 color: Colors.grey,
-
+                letterSpacing: 2.0
               ),
-            )
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Mbugua-Caleb',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'Current Ninja Level',
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '8',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'mbuguacaleb30@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+
           ],
         ),
       ),
